8fc386a2be899a761bc65f19d5babfafa26e6290
theGeekkit <111545346+theGeekkit@users.noreply.github.com>
2022-09-26 19:56:39 -0500

    Co-authored-by: kylesteinmann <kylesteinmann@users.noreply.github.com> Co-authored-by: chrisHembree <chrisHembree@users.noreply.github.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { TaskComponent } from './Tasks/task/task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TaskComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+     MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]

bf315c7d70b2f202283dc6650796e094f5d1dd92
theGeekkit <111545346+theGeekkit@users.noreply.github.com>
2022-09-29 17:53:53 -0500

    9-29-22

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { TaskElementComponent } from './Tasks/task-element/task-element.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    TaskElementComponent,
 
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

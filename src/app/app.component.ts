363162822dbdf7b03ddafb962ff2e98f5f59a2b8
theGeekkit <111545346+theGeekkit@users.noreply.github.com>
2022-09-29 17:53:40 -0500

    9-29-22

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'To-Do-Angular';
+  taskElements = [];
+  newTaskName = '';
+  newTaskContent = '';
+
+  onTaskAdded(taskData: {taskName: string, taskContent: string}) {
+    this.taskElements.push({
+      type: 'task',
+      name: this.newTaskName,
+      content: this.newTaskContent
+    });
 }

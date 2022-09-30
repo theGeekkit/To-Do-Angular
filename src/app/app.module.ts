3df17988897f36aba178a00441a68902174669d5
theGeekkit <111545346+theGeekkit@users.noreply.github.com>
2022-09-29 19:28:14 -0500

    crazy heath 9-29-22

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,26 @@ import { HeaderComponent } from './header/header.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { TaskElementComponent } from './Tasks/task-element/task-element.component';
+import { TaskListComponent } from './Tasks/task-list/task-list.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     TaskElementComponent,
+    TaskListComponent,
 
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    FormsModule
+    FormsModule,
+    MatCardModule,
+    MatGridListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

c8c00508d369118c341c32bb0ff782db1eddc905
theGeekkit <111545346+theGeekkit@users.noreply.github.com>
2022-10-13 20:03:46 -0500

    Co-authored-by: chrisHembree <chrisHembree@users.noreply.github.com> Co-authored-by: kylesteinmann <kylesteinmann@users.noreply.github.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,7 @@ import { TaskListComponent } from './Tasks/task-list/task-list.component';
     BrowserAnimationsModule,
     // MatToolbarModule,
     FormsModule,
+
     // MatCardModule,
     // MatGridListModule,
   ],

78078bf26634a292525ba5fab09f50515c89c970
theGeekkit <sonofgondolin16@gmail.com>
2022-11-19 20:56:04 -0600

    11-19-22

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  {path: 'todoForm', component:TodoFormComponent}
-  {path: 'login', component:LoginComponent}
+  {path: 'todoForm', component:TodoFormComponent},
+  {path: 'login', component:LoginComponent},
+  {path: '', component:TodoFormComponent}
 ]
 
 
